743cdf13aac2213239476345c48054e2637669c6
brandonbouley <brandonbouley@billericak12.com>
2019-12-03 23:14:24 -0500

    yah YEET

--- src/loops.java
+++ src/loops.java
@@ -1,3 +1,4 @@
+// brandon bouley !! :D
 public class loops {
 
     public static int addOdds(int n){
@@ -56,24 +57,72 @@ public class loops {
         return total;
     }
 
-    public static void sillyNumbers(){
-        int j=0;
-        for (int h=0; h<10; h++, j++)
-            for (int i=0; i<3; i++)
-            {
-                System.out.print(j);
-            }
+    public static void sillyNumbers() {
+
+        for (int g = 0; g < 3; g++) {
+            int j = 0;
+            for (int h = 0; h < 10; h++, j++)
+                for (int i = 0; i < 3; i++) {
+                    System.out.print(j);
+                }
+            System.out.println();
 
 
         }
+    }
 
-    public static void sillyNumbers2(){
-        int j=9;
-        for (int h=0; h<10; h++, j--)
-            for (int i=j; i>0; i--)
-            {
-                System.out.print(j);
+    public static void sillyNumbers2() {
+
+        for (int g = 0; g < 3; g++) {
+            int j = 9;
+            for (int h = 0; h < 10; h++, j--)
+                for (int i = j; i > 0; i--) {
+                    System.out.print(j);
+                }
+            System.out.println();
+
+
+        }
+    }
+
+    public static void  dollarsAndStars(){
+        int outStar=0;
+        int dollar=7;
+        int inStar=14;
+
+
+        for (int a=0; a<7; a++){
+            for (int b=0; b<outStar; b++){
+                System.out.print("*");
             }
+
+
+            for (int c=0; c<dollar; c++){
+                System.out.print("$");
+
+            }
+
+
+            for (int d=0; d<inStar; d++){
+                System.out.print("*");
+            }
+            inStar-=2;
+
+            for (int c=0; c<dollar; c++){
+                System.out.print("$");
+
+            }
+            dollar--;
+
+            for (int b=0; b<outStar; b++){
+                System.out.print("*");
+            }
+            outStar+=2;
+
+            System.out.println();
+
+        }
+
 
 
     }
@@ -94,6 +143,9 @@ public class loops {
         sillyNumbers();
         System.out.println();
         sillyNumbers2();
+        System.out.println();
+        dollarsAndStars();
+
 
 
 
@@ -107,3 +159,32 @@ public class loops {
 
     }
 }
+/*
+25
+2500
+1
+0
+
+1
+3
+
+5
+2
+28
+
+000111222333444555666777888999
+000111222333444555666777888999
+000111222333444555666777888999
+
+999999999888888887777777666666555554444333221
+999999999888888887777777666666555554444333221
+999999999888888887777777666666555554444333221
+
+$$$$$$$**************$$$$$$$
+**$$$$$$************$$$$$$**
+****$$$$$**********$$$$$****
+******$$$$********$$$$******
+********$$$******$$$********
+**********$$****$$**********
+************$**$************
+ */
